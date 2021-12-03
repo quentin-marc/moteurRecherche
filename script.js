e19918c4ed8f1905c100416b8fc9754ac55ca36c
quentin-marc <quentinmarc81@gmail.com>
2021-12-03 08:44:02 +0100

    template

---
+++ script.js
@@ -0,0 +1,9 @@
+/*   Appelé avant que le HTML soit chargé    */
+console.log("js scripts here.");
+
+/*   Appele une fois le HTML chargé    */
+window.onload = actOnWindow;
+
+function actOnWindow(){
+    console.log('Lister dans cette fonction toute les fonctions qui peuvent agir sur la page HTML');
+}
