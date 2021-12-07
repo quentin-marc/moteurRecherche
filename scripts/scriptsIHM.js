85f66a38d36a93c660fa68925089cceae6e43ba9
quentin-marc <quentinmarc81@gmail.com>
2021-12-07 11:42:38 +0100

    delete coucou

--- scripts/scriptsIHM.js
+++ scripts/scriptsIHM.js
@@ -1,10 +1,3 @@
-window.onload = displaySeeMore;
-
-function displaySeeMore(){
-    alert("coucou");
-}
-
-
 // affiche le champ entier associé au "see more"
 function seeMore(){
     document.getElementById("description").style.height = "auto";
