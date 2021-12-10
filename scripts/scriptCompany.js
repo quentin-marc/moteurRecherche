1336638c696875ee5d24e6ae6fcb60a12db57cc7
Brahim-Mahmoudi <petitbra@live.fr>
2021-12-10 10:57:56 +0100

    undo

--- scripts/scriptCompany.js
+++ scripts/scriptCompany.js
@@ -4,6 +4,7 @@ function actOnWindow(){
     //companyRequest("http://dbpedia.org/resource/Microsoft")
 
     var undo = JSON.parse(sessionStorage.getItem('undo'))
+    var founder = sessionStorage.getItem('Company')
 
     var uriUndo = {
         type : "Company",
